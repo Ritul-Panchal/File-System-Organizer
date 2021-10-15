f4849c9c730e3d9fd9af6de27732662be164e2b3
Ritul <ritulpanchal2709@gmail.com>
2021-10-15 17:22:45 +0530

    code

---
+++ main.js
@@ -0,0 +1,21 @@
+let helpObj = require('./commands/help');
+let treeObj = require('./commands/tree');
+let organizerObj = require('./commands/organizer');
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+let path = inputArr[1];
+
+switch(command){
+    case "help":
+        helpObj.helpFn();
+        break;
+    case "tree":
+        treeObj.treeFn(path);
+        break;
+    case "organizer":
+        organizerObj.organizeFn(path);
+        break;
+    default:
+        console.log("Invalid command");
+        break;
+}
